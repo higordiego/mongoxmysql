109d125fab0c370d6fbe6d1b0288085a97142129
Alison <alisonmonteiro.10@gmail.com>
2016-03-05 20:01:08 -0300

    Fix mysql.js indentation

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -13,36 +13,36 @@ var connectionConfig = {
 var mysqlDb = {};
 
 mysqlDb.insert = function (dataSize, done) {
-	var task = [];
+  var task = [];
   var connection = mysql.createConnection(connectionConfig);
 
-	task.push(function (callback) {
-		connection.connect(function () {
-			callback();
-		});
-	});
+  task.push(function (callback) {
+    connection.connect(function () {
+      callback();
+    });
+  });
 
-	task.push(function (callback) {
-		connection.query('CREATE DATABASE IF NOT EXISTS palestra', function () {
-			callback();
-		});
-	});
+  task.push(function (callback) {
+    connection.query('CREATE DATABASE IF NOT EXISTS palestra', function () {
+      callback();
+    });
+  });
 
-	task.push(function (callback) {
-		connection.query('DROP TABLE usuario', function (data, err) {
-			callback();
-		});
-	});
+  task.push(function (callback) {
+    connection.query('DROP TABLE usuario', function () {
+      callback();
+    });
+  });
 
-	task.push(function (callback) {
-		connection.query('DROP TABLE time', function (data, err) {
-			callback();
-		});
-	});
+  task.push(function (callback) {
+    connection.query('DROP TABLE time', function () {
+      callback();
+    });
+  });
 
 
-	task.push(function (callback) {
-		connection.query('CREATE TABLE IF NOT EXISTS usuario (' +
+  task.push(function (callback) {
+    connection.query('CREATE TABLE IF NOT EXISTS usuario (' +
       'id int(11) NOT NULL AUTO_INCREMENT,' +
       'nome varchar(32) NOT NULL,' +
       'pontuacao int(11) NOT NULL,' +
@@ -50,75 +50,75 @@ mysqlDb.insert = function (dataSize, done) {
       'time int(10) unsigned NOT NULL,' +
       'PRIMARY KEY (id),' +
       'KEY time (time)' +
-    ')',
+      ')',
 
-    function () {
-      callback();
-    });
-	});
+      function () {
+        callback();
+      });
+  });
 
-	task.push(function (callback) {
-		connection.query('CREATE TABLE IF NOT EXISTS time (' +
+  task.push(function (callback) {
+    connection.query('CREATE TABLE IF NOT EXISTS time (' +
       'id int(11) NOT NULL AUTO_INCREMENT,' +
       'nome varchar(32) NOT NULL,' +
       'pais varchar(32) NOT NULL,' +
       'cidade varchar(32) NOT NULL,' +
       'PRIMARY KEY (id)' +
-    ')',
+      ')',
 
-    function () {
-			callback();
-		});
-	});
+      function () {
+        callback();
+      });
+  });
 
-	for (var i = 0; i < dataSize; i++) {
-		task.push(function (callback) {
-			connection.query('INSERT INTO usuario (nome, email, time, pontuacao) VALUES (?, ?, ?, ?)', [
+  for (var i = 0; i < dataSize; i++) {
+    task.push(function (callback) {
+      connection.query('INSERT INTO usuario (nome, email, time, pontuacao) VALUES (?, ?, ?, ?)', [
         faker.Name.findName(),
         faker.Internet.email(),
         Math.floor(Math.random() * dataSize),
         Math.floor(Math.random() * 1000)
       ], function (err) {
-				if (!err) {
-					return callback();
-				} else {
-					console.log('User insert error', err);
-				}
-			})
-		});
+        if (!err) {
+          return callback();
+        } else {
+          console.log('User insert error', err);
+        }
+      })
+    });
 
-		task.push(function (callback) {
-			connection.query('INSERT INTO time (nome, pais, cidade) VALUES (?, ?, ?)', [
+    task.push(function (callback) {
+      connection.query('INSERT INTO time (nome, pais, cidade) VALUES (?, ?, ?)', [
         faker.Address.city(),
         faker.Address.ukCountry(),
-				faker.Company.companyName().split(" ")[0].split(",")[0]
+        faker.Company.companyName().split(" ")[0].split(",")[0]
       ], function (err) {
-				if (!err) {
-					return callback();
-				} else {
-					console.log('Time insert error', err);
-				}
-			})
-		});
-	}
+        if (!err) {
+          return callback();
+        } else {
+          console.log('Time insert error', err);
+        }
+      })
+    });
+  }
 
-	console.time('mysql insert');
+  console.time('mysql insert');
 
-	async.series(task, function () {
-		console.timeEnd('mysql insert');
-		connection.end();
+  async.series(task, function () {
+    console.timeEnd('mysql insert');
+    connection.end();
 
-		if (done) done();
-	});
+    if (done) done();
+  });
 };
 
 
 mysqlDb.find = function (dataSize, done) {
-	var task = [];
-	var connection = mysql.createConnection(connectionConfig);
+  var task = [];
+  var connection = mysql.createConnection(connectionConfig);
 
-	task.push(function (callback) {
-		connection.query('select * from usuario inner join time on usuario.time = time.id', function (data, err) {
+  task.push(function (callback) {
+    connection.query('select * from usuario inner join time on usuario.time = time.id', function (data, err) {
       for (var i in data) {
         if (!data[i]['name']) {
           console.log('Mysql error: Team unnamed.');
@@ -126,20 +126,18 @@ mysqlDb.find = function (dataSize, done) {
       }
 
       return callback();
-		})
-	});
+    })
+  });
 
-	console.time('mysql select');
-	async.series(task, function () {
-		console.timeEnd('mysql select');
-		connection.end();
+  console.time('mysql select');
+  async.series(task, function () {
+    console.timeEnd('mysql select');
+    connection.end();
 
-		if (done) done();
-	});
+    if (done) done();
+  });
 };
 
 module.exports = function (type, dataSize, done) {
-	return mysqlDb[type](dataSize, done);
+  return mysqlDb[type](dataSize, done);
 };
-
-
