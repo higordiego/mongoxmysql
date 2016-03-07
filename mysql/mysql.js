36f7201f743c37914bfb8b54481b65dfc58380ab
Higor Diego <higordiegoti@gmail.com>
2016-03-07 19:38:47 -0300

    Banco de dados

--- mysql/mysql.js
+++ mysql/mysql.js
@@ -7,7 +7,7 @@ var metodo = {};
 var conexao      =    mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : 'i190f200r', // Senha do
+	password : '', // Senha do
 	database : 'palestra',
 	debug    :  false
 });
